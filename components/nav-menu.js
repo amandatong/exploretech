a18644a7e658ecd56c111412968c51f8d299e8d0
Amanda Tong <amanda.g.tong@gmail.com>
2022-07-28 07:39:12 +0900

    added explore page basics

--- components/nav-menu.js
+++ components/nav-menu.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { useTranslation } from 'next-i18next';
 import { Drawer } from 'antd';
+import { XIcon } from '@heroicons/react/outline';
 
 export default function NavMenu({onClose, visible}) {
   const { t, i18n } = useTranslation('common', { keyPrefix: 'nav' });
@@ -10,26 +11,29 @@ export default function NavMenu({onClose, visible}) {
   const onMouseEnter = (key) => setIsHovered(key);
   const onMouseLeave = () => setIsHovered(false);
 
-  const bg = "'/assets/nav/home.svg'"
+  const bg = "'/assets/nav/blank.svg'"
 
   return(
     <Drawer placement="right" onClose={onClose} visible={visible} width="100vw" mask={false} 
+    closable={false}
       drawerStyle={{
-        backgroundImage: `url("${isHovering ? `/assets/nav/${isHovering}.svg` : ''}")`
+        backgroundImage: `url("${isHovering ? `/assets/nav/${isHovering}.svg` : '/assets/nav/blank.svg'}")`
       }}>
-        <div className="menu-container" >
-          <div className="links">
-          {['home', 'explore', 'future'].map((key) =>{
-            return(
-              <div id={key} class="link" onMouseEnter={() => onMouseEnter(key)} onMouseLeave={onMouseLeave} onClick={onClose}><Link href={`/${key === 'home' ? '' : key}`}>{t(key)}</Link></div>
-            )
-          })}
-            {/* <div id="home" onMouseEnter={() => onMouseEnter('home')} onMouseLeave={onMouseLeave} onClick={onClose}><Link href="/">{t('home')}</Link></div>
-            <div id="explore" onMouseEnter={() => onMouseEnter('explore')} onMouseLeave={onMouseLeave} onClick={onClose}><Link href="/explore">{t('explore')}</Link></div>
-            <div id="future" onMouseEnter={() => onMouseEnter('future')} onMouseLeave={onMouseLeave} onClick={onClose}><Link href="/future">{t('future')}</Link></div> */}
+        <div class="menu-wrap">
+          <div className="closeBar">
+              <div id="close" onClick={onClose}><XIcon/></div>
           </div>
+          <div className="menu-container">
+            <div className="links">
+              {['home', 'explore', 'future'].map((key) =>{
+                return(
+                  <div id={key} key={key} class="link" onMouseEnter={() => onMouseEnter(key)} onMouseLeave={onMouseLeave} onClick={onClose}><Link href={`/${key === 'home' ? '' : key}`}><a class={key}>{t(key)}</a></Link></div>
+                )
+              })}
+            </div>
 
-          
+            
+          </div>
         </div>
     </Drawer>
   )
--- pages/explore.js
+++ pages/explore.js
@@ -1,20 +1,36 @@
 import Link from 'next/link';
+import AICube from '../public/assets/explore/ai.svg';
+import MetaverseCube from '../public/assets/explore/metaverse.svg';
+import DTCube from '../public/assets/explore/dt.svg';
 import { useTranslation } from 'next-i18next';
 
-export default function Second() {
-    const { t, i18n } = useTranslation('common', { keyPrefix: 'home' });
+export default function Explore() {
+    const { t, i18n } = useTranslation('common', { keyPrefix: 'explore' });
+
     return (
-        <>
-        <Link href="" locale="en">English</Link> <Link href="" locale="ko">Korean</Link>
-          {t('subtitle')}
-        </>
-      )
+      <main id="explore">
+        <div className="page-title">{t('title')}</div>
+
+        <div className="links-wrap">
+          {[{key:'ai',cube:<AICube/>},{key:'metaverse', cube:<MetaverseCube/>},{key:'digital-twin',cube:<DTCube/>}].map((topic) => {
+          return(
+            <Link key={topic.key} href={`/${topic.key}`} passHref>
+              <div id={topic.key} className="link">
+                <div className="cube">{topic.cube}</div>
+                <div className="link-title">{t(topic.key)}</div>
+              </div>
+            </Link>
+          )
+          })}
+        </div>
+      </main>
+    )
 
 }
 
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 export const getStaticProps = async ({ locale }) => ({
-    props: {
-      ...(await serverSideTranslations(locale, ['common']))
-    }
-  })
+  props: {
+    ...(await serverSideTranslations(locale, ['common']))
+  }
+})
