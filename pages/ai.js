d796b3613e982420357b3caf6878229e7cc92b2a
amandatong <amanda.g.tong@gmail.com>
2022-08-10 17:03:35 +0900

    finished adding machine learning info and translation

--- pages/ai.js
+++ pages/ai.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { useState } from 'react';
 import { useTranslation } from 'next-i18next';
 import TopicMenu from '../components/topic-menu';
+import { ArrowLeftIcon } from '@heroicons/react/outline';
 import { ArrowRightIcon } from '@heroicons/react/outline';
 import NeuralNet from '../public/assets/ai/neural_net.svg';
 
@@ -9,11 +10,55 @@ import dynamic from 'next/dynamic';
 const DynamicLordIcon = dynamic(() => import('../components/LordIcon'), {
     ssr: false
 });
+
 const icons_width = {width:'16vw', height:'16vw'};
+const ML_STEPS = 7;
+const ml_icons = [
+    ['puzzle', 'system'],
+    ['animals','cat'],
+    ['pets','cat'],
+    ['pets','cat'],
+    ['pets','cat'],
+    ['pets','cat'],
+    ['pets','cat'],
+]
+
 
 export default function AI() {
     const { t, i18n } = useTranslation(['ai']);
     const [showBottom, setShowBottom] = useState(false);
+    const [MLIndex, setMLIndex] = useState(0);
+    const ml_cards = [];
+
+    const nextSlide = () => { 
+        let new_index = MLIndex + 1;
+        if (new_index == ML_STEPS) {
+            new_index = 0;
+        }
+        setMLIndex(new_index);
+    }
+    const prevSlide = () => { 
+        let new_index = MLIndex - 1;
+        if (new_index == -1) {
+            new_index = ML_STEPS - 1;
+        }
+        setMLIndex(new_index);
+    }
+
+    for (let index = 0;index < ML_STEPS;index++){
+        ml_cards.push(
+            <div className={index === MLIndex ? 'show' : 'hide'}>
+                <div className="section-title">{index !== 0 && `${index}.`} {t(`ml.steps.${index}.title`)}</div>
+                <div className="desc">
+                    <p>{t(`ml.steps.${index}.desc`)}</p>
+                </div>
+                <div className="button-wrap">
+                    {index !== 0 && <div className="ex-button" onClick={() => prevSlide()}><ArrowLeftIcon/> {t('ml.prev')}</div>}
+                    <div className="ex-button" onClick={() => nextSlide()}>{index === 0 ? t('ml.how') : (index === (ML_STEPS - 1) ? t('ml.finish') : t('ml.next'))} <ArrowRightIcon/></div>
+                </div>
+            </div>
+        )
+    }
 
     const showBottomNav = () => {
         setShowBottom(true);
@@ -51,18 +96,16 @@ export default function AI() {
                 </div>
             </div>
             <div className="ml_intro_wrap">
+
                 <div className="ml_intro">
-                    <div className="section-title">{t('ml.title')}</div>
-                    <div className="desc">
-                        <p>{t('ml.desc')}</p>
-                    </div>
-                    <div className="ex-button">how it works <ArrowRightIcon/></div>
+                    {ml_cards}
                 </div>
+
                 <div className="ml_cards">
                     <div className="cards one">
                         <div className="card one">
                             <div className="animal-contain">
-                                <lord-icon trigger="loop" speed="0.3" src="/assets/lotties/horse.json" style={icons_width}/>
+                                <lord-icon trigger="boomerang" speed="0.3" src={`/assets/lotties/${ml_icons[MLIndex][0]}.json`} style={icons_width}/>
                             </div>
                         </div>
                         <div className="card two"></div>
@@ -71,7 +114,7 @@ export default function AI() {
                     <div className="cards two">
                         <div className="card one">
                             <div className="animal-contain">
-                                <lord-icon trigger="loop" speed="0.4" src="/assets/lotties/horse_2.json" style={icons_width}/>
+                                <lord-icon trigger="loop" speed="0.4" src={`/assets/lotties/${ml_icons[MLIndex][1]}.json`} style={icons_width}/>
                             </div>
                         </div>
                         <div className="card two"></div>
@@ -79,8 +122,11 @@ export default function AI() {
                     </div>
                 </div>
             </div>
-            <div className="machine-learning">
-                hello
+            <div className="application">
+                <div className="section-title">{t('role.title')}</div>
+                    <div className="desc">
+                        <p>{t('role.desc')}</p>
+                    </div>
             </div>
             <Link href="/metaverse" passHref>
                 <div className={`reveal_next ${showBottom ? 'show' : 'hide'}`}>{t('next')} <ArrowRightIcon/></div>
